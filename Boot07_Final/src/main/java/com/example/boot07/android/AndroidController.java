b8d04197fb33afbf876464a5eda7ed479e6a9998
kimsunjae <ksj100301@gmail.com>
2023-08-02 10:30:39 +0900

    Boot07_Final android Controller added

--- Boot07_Final/src/main/java/com/example/boot07/android/AndroidController.java
+++ Boot07_Final/src/main/java/com/example/boot07/android/AndroidController.java
@@ -6,14 +6,47 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.boot07.cafe.dao.CafeDao;
 import com.example.boot07.cafe.dto.CafeDto;
+import com.example.boot07.gallery.dao.GalleryDao;
+import com.example.boot07.gallery.dto.GalleryDto;
 
 
 // 컨트롤러 케소드에서 리턴하는 데이터가 바로 클라이언트에게 응답되는 컨트롤러
 @RestController
 public class AndroidController {
+	
+	@Autowired
+	private GalleryDao galleryDao;
+	
+	@GetMapping("/android/fortue")
+    public Map<String, Object> fortune(){
+        Map<String, Object> map=new HashMap<String, Object>();
+        map.put("fortuneToday", "동쪽으로 가면 귀인을 만나요");
+        return map;
+    }
+   
+    private List<String> names=new ArrayList();
+   
+    @PostMapping("/android/insert")
+    public List<String> insert(String name){
+        names.add(name);
+        return names;
+    }
+	
+	@GetMapping("/android/gallery/list")
+	public List<GalleryDto> galleryList(){
+		
+		// 20 개면 select 해오도록 GalleryDto 에 정보 담기
+		GalleryDto dto = new GalleryDto();
+		dto.setStartRowNum(1);
+		dto.setEndRowNum(20);
+		
+		// GalleryDao 객체가 리턴해주는 데이터를 바로 리턴하기
+		return galleryDao.getList(dto);
+	}
 	
 	/*
 	 * String Type 을 리턴하면 리턴된 String 의 내용이 그대로 응답한다.
